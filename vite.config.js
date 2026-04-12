79f873ae9d9687854b14e6d414d1a7d55d7f568f
Your Name <you@example.com>
2026-04-12 17:17:51 +0200

    Fix caching with Vercel headers

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,5 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    rollupOptions: {
-      output: {
-        entryFileNames: `assets/[name]-[hash]-${Date.now()}.js`,
-      }
-    }
-  }
+  plugins: [react()],
 })
